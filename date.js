a34c0f7e67e6ce79cf7a4a181772ec39d03100e6
snigdhcinu <47975136+snigdhcinu@users.noreply.github.com>
2020-05-25 00:41:14 +0530

    our date module
    We will be using our own date module, to refactor the code.

---
+++ date.js
@@ -0,0 +1,13 @@
+exports.getDate=function(){
+var today=new Date(); // day is in no. format.
+	var day;
+	var work;
+	var options={
+		weekday:'long',
+		day:'numeric',
+		month:'long'
+	};
+	return today.toLocaleDateString("en-US",options);	// To set today's day and date.
+	// See stackoverflow for more information on how to change the date info to string.
+
+}

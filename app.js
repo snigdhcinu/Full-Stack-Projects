a3d263f15f87f1df148c54c1f748e013ec1cf76d
snigdhcinu <snigdhshourya11111998@gmail.com>
2020-12-10 23:54:52 +0530

    adding routes and database for storing images and other user details

--- app.js
+++ app.js
@@ -1,45 +1,56 @@
-const multer = require('multer')
 const express = require('express')
 const ejs = require('ejs')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
-
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.set('view engine','ejs')
 app.use(express.static('public'));
-// mongoose.connect('mongodb://localhost:27017/bbankDB',
-// 	{useNewUrlParser:true,useUnifiedTopology:true})
-let password = process.env.PASSWORD;
 
 
-mongoose.connect('mongodb://localhost:27017/profileDB',
-	{useNewUrlParser:true,useUnifiedTopology:true})
-// mongoose.connect(`mongodb+srv://root:${password}@cluster0.bxeya.mongodb.net/bbankDB?retryWrites=true&w=majority`,
-// 	{useNewUrlParser:true,useUnifiedTopology:true})
 
 
-// Hospital model & schema
+mongoose.connect('mongodb://localhost:27017/profileDB', {useNewUrlParser:true,useUnifiedTopology:true})
+
+
+
+// Profile model & schema
 
 const profileSchema = new mongoose.Schema({
+	dp:{ data: Buffer, contentType: String },
 	name:String,
-	// id will serve as password, since unique to every hospital
-	id:String,
-	address:String,
-	city:String,
-	state:String,
+	occ:String,
+	add:String,
 	phno:Number,
-	avail:{
-		Apos:Number,
-		Aneg:Number,
-		Bpos:Number,
-		Bneg:Number,
-		Opos:Number,
-		Oneg:Number,
-		ABpos:Number,
-		ABneg:Number
-	}
-})
+	gender:String,
+	pref:String
+});
 
 const Profile = mongoose.model('Profile',profileSchema)
+
+
+										// Home Route
+
+app.route('/')
+	.get((req,res)=>{
+		res.redirect('/form');
+	})
+
+										// Form Route
+
+app.route('/form')
+	.get((req,res)=>{
+		res.render('form');
+	})
+
+	.post( upload.single('dp'),(req,res)=>{
+		console.log(req.file)
+	})
+
+
+app.listen('8000',()=>{
+	console.log('Server online on port 8000')
+})
